ab89d7b73ab9b9f17d2ceaf862cb32b916354573
Steven Santiago <stevensantiago@Stevens-MacBook-Air.local>
2017-01-28 22:49:50 -0800

    Completed Weather App

---
+++ ShineCloudRain/Location.swift
@@ -0,0 +1,9 @@
+//
+//  Location.swift
+//  ShineCloudRain
+//
+//  Created by Steven Santiago on 1/28/17.
+//  Copyright © 2017 Steven Santiago. All rights reserved.
+//
+
+import Foundation
---
+++ ShineCloudRain/WeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.swift
+//  ShineCloudRain
+//
+//  Created by Steven Santiago on 1/28/17.
+//  Copyright © 2017 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
